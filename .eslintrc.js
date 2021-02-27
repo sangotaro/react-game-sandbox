081d626fcfdc3535e9241bea52ac3dfceba45d72
Shotaro Watanabe <shotaro.watanabe0715@gmail.com>
2021-02-27 17:54:40 +0900

    feat: r3f sample (#6)

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   rules: {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
   },
   settings: {
     react: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,16 @@
-import React, { useState } from "react";
-import "./App.css";
-import logo from "./logo.svg";
+import React from "react";
+import { Box } from "./components/box";
+import { Game } from "./components/game";
 
-const App: React.VFC = () => {
-  const [count, setCount] = useState(0);
-
+export const App: React.VFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <Game>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Game>
     </div>
   );
 };
-
-export default App;
